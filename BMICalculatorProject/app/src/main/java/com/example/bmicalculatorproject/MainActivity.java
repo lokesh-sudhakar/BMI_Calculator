8485761fc5da14bf4217c94d145eb854fe800753
Lokesh <chennamchettyl@gmail.com>
2019-06-14 10:00:09 +0530

    first cut UI completed logic remaining

--- BMICalculatorProject/app/src/main/java/com/example/bmicalculatorproject/MainActivity.java
+++ BMICalculatorProject/app/src/main/java/com/example/bmicalculatorproject/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.bmicalculatorproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView heightEntry;
+    private TextView weightEntry;
+    private LinearLayout numberPad;
+    private LinearLayout resultView;
+    private Button buttonGO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        heightEntry=(TextView)findViewById(R.id.height_entry);
+        weightEntry=(TextView)findViewById(R.id.weight_entry);
+        numberPad=(LinearLayout)findViewById(R.id.numpad_view);
+        resultView=(LinearLayout)findViewById(R.id.result_view);
+        buttonGO=(Button)findViewById(R.id.btn_go);
+        heightEntry.setOnClickListener(this);
+        buttonGO.setOnClickListener(this);
     }
+    public void onClick(View v)
+    {
+        if(v==heightEntry){
+            numberPad.setVisibility(View.VISIBLE);
+            resultView.setVisibility(View.GONE);
+            heightEntry.setTextColor(getResources().getColor(R.color.colorOrange));
+            weightEntry.setTextColor(getResources().getColor(R.color.colorDarkGrey));
+        }
+        if (v==buttonGO){
+            resultView.setVisibility(View.VISIBLE);
+            numberPad.setVisibility(View.GONE);
+        }
+    }
+    public View getactiveTextView(){
+        if(heightEntry.getCurrentTextColor()-(16777215+1)==R.color.colorOrange){
+            return heightEntry;
+        }else{
+            return  weightEntry;
+        }
+    }
+
+
+
 }
